0627325511300cd7acb1df5c5f88294d77567f13
loicgasser <loicgasser4@gmail.com>
2016-02-09 17:02:01 +0000

    Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-import io
 import os
 from setuptools import setup, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with io.open(here + '/README.md') as f:
-    long_description = f.read()
 requires = open(os.path.join(here, 'requirements.txt')).read().split('\n')
 
 setup(name='poolmanager',
-      version='0.0.2',
+      version='0.0.3',
       description='Simple pool manager',
-      long_description=long_description,
       classifiers=[],
       keywords='',
       author='Loic Gasser',
@@ -20,7 +16,7 @@ setup(name='poolmanager',
       url='https://github.com/loicgasser/poolmanager',
       packages=find_packages(exclude=['tests']),
       package_dir={'poolmanager': 'poolmanager'},
-      package_data={'poolmanager': ['*.md']},
+      package_data={'': ['*.txt', '*.md']},
       include_package_data=True,
       zip_safe=False,
       test_suite='nose.collector',
