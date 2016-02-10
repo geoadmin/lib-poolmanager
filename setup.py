308cb97b83f9483e038c3b4079fdbbeba9e3be2b
loicgasser <loicgasser4@gmail.com>
2016-02-10 10:11:19 +0000

    Attempt to include package data

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-import os
 from setuptools import setup, find_packages
 
 
-here = os.path.abspath(os.path.dirname(__file__))
-requires = open(os.path.join(here, 'requirements.txt')).read().split('\n')
 
 setup(name='poolmanager',
-      version='0.0.3',
+      version='0.0.4',
       description='Simple pool manager',
       classifiers=[],
       keywords='',
@@ -16,11 +13,10 @@ setup(name='poolmanager',
       url='https://github.com/loicgasser/poolmanager',
       packages=find_packages(exclude=['tests']),
       package_dir={'poolmanager': 'poolmanager'},
-      package_data={'': ['*.txt', '*.md']},
       include_package_data=True,
       zip_safe=False,
       test_suite='nose.collector',
-      install_requires=requires
+      install_requires=[],
       )
       
 
